5fc57b68f9f6d06e74f39d4bd0b9ae2d66b35eb8
Vitor Mateus <vmateusaguiar@gmail.com>
2022-08-16 18:43:46 -0300

    atualizando header

--- pages/components/Cabecalho/style.js
+++ pages/components/Cabecalho/style.js
@@ -1,24 +1,27 @@
 import styled from "styled-components";
 
 export const Header = styled.div`
-        width: 136.7rem;
+        width: 100%;
         height: 17.7rem;
         background-color: black;
 
-    h1 {
-        color: white;
-        margin-top: 3.7rem;
-        padding-right: 31.2rem;
-        font-size: 3.6rem;
-    }
+    
 
     .header_sup {
         display: flex;
         height: 9.5rem;
-        width: 136.7rem;
+        width: 100%;
         align-items: center;
         margin-bottom: 0.1rem;
         justify-content: flex-end;
+    }
+
+    h1 {
+        display: flex;
+        justify-content: center;
+        color: white;
+        font-size: 3.6rem;
+        width: 100%;
     }
 
     .botao_usuario {
@@ -28,27 +31,28 @@ export const Header = styled.div`
         margin-right: 4.2rem;
     }
 
-    h1 {
-        margin-right: 9.5rem;
-    }
-
     .linha_branca {
         height: 0.1rem;
-        width: 136.7rem;
+        width: 100%;
         background-color: white;
     }
 
     .header_inf {
         display: flex;
-        justify-content: end;
         height: 8rem;
+        width: 100%;
+    }
+
+    .search {
+        display: flex;
+        justify-content: end;
+        margin-right: 4.2rem;
     }
 
     .pesquisar {
         width: 41.5rem;
         height: 4.708rem;
         margin-top: 1.2rem;
-        margin-left: 91rem;
         border-radius: 1.6rem;
         position: absolute;
         
@@ -57,7 +61,7 @@ export const Header = styled.div`
     .botao_lupa {
         border: none;
         background-color: transparent;
-        margin-left: 126.8rem;
+        margin-right: 1rem;
         margin-top: 1.7rem;
         position: relative;
     }
